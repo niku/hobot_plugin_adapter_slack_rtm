62efabf4aa8a768ebb3f14f37ee13ccb08fd1749
niku <niku@niku.name>
2018-05-05 09:08:43 +0900

    Add stream_data for propety based testing

--- test/hobot_plugin_adapter_slack_rtm_test.exs
+++ test/hobot_plugin_adapter_slack_rtm_test.exs
@@ -1,8 +1,17 @@
 defmodule Hobot.Plugin.Adapter.SlackRTMTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use ExUnitProperties
   doctest Hobot.Plugin.Adapter.SlackRTM
 
   test "greets the world" do
     assert Hobot.Plugin.Adapter.SlackRTM.hello() == :world
+  end
+
+  property "sort two values" do
+    check all term1 <- term(),
+              term2 <- term() do
+      [a, b | []] = Enum.sort([term1, term2])
+      assert a <= b
+    end
   end
 end
